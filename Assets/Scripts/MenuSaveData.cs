d1632cc03303c294625fc73e8dc802d8b9a29689
jfcarocota <jfcarocota@gmail.com>
2018-12-04 21:23:31 -0700

    menu save data

---
+++ Assets/Scripts/MenuSaveData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.MemorySystem;
+
+public class MenuSaveData : MonoBehaviour
+{
+    [SerializeField]
+    List<GameDataButton> gameButtons;
+
+    void StartButtons()
+    {
+        foreach(GameDataButton gdb in gameButtons)
+        {
+            gdb.startButton();
+        }
+    }
+
+    private void Start()
+    {
+        StartButtons();
+
+        List<string> games = MemorySystem.LookForGames;
+
+        for (int i = 0; i < games.Count; i++)
+        {
+            foreach(GameDataButton gdb in gameButtons)
+            {
+                if (gdb.buttonDefaultGameName == games[i])
+                {
+                    gdb.setGameName = games[i];
+                }
+            }
+
+            //gameButtons[i].setGameName = games[i];
+        }
+    }
+}
